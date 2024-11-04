672f5a949b5cadcaea9a412b0efd654438e9583a
Kakayou <shigaev_1990@mail.ru>
2024-11-04 15:24:29 +0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+
+  return (
+      <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <h1>Счетчик: {count}</h1>
+        <button onClick={increment} style={{ marginRight: '10px' }}>Увеличить</button>
+        <button onClick={decrement}>Уменьшить</button>
+      </div>
+  );
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Counter className="counter" />
+        </header>
+      </div>
   );
 }
 
